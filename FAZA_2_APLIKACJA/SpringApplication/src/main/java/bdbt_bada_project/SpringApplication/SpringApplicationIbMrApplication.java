d8511ea63c869643be397adc59044afe32a7c100
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-12-28 01:10:16 +0100

    Wygenerowane pliki SpringApplication z Spring Initializr

---
+++ FAZA_2_APLIKACJA/SpringApplication/src/main/java/bdbt_bada_project/SpringApplication/SpringApplicationIbMrApplication.java
@@ -0,0 +1,13 @@
+package bdbt_bada_project.SpringApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationIbMrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationIbMrApplication.class, args);
+	}
+
+}
