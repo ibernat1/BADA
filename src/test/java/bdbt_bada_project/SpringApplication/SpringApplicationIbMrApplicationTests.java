550f2e68fd014b5fd6a864886b6d296e6ade9b2d
ibernat <iga.bernat.stud@pw.edu.pl>
2024-01-04 13:51:24 +0100

    prezentacja 2.1

---
+++ src/test/java/bdbt_bada_project/SpringApplication/SpringApplicationIbMrApplicationTests.java
@@ -0,0 +1,13 @@
+package bdbt_bada_project.SpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationIbMrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
