62de76f32b60b649618b5919406264a75e5141b5
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2024-01-08 01:26:37 +0100

    Testy jednostkowe dla update zwierzę

--- FAZA_2_APLIKACJA/SpringApplication/src/test/java/bdbt_bada_project/SpringApplication/ZwierzetaDAOTest.java
+++ FAZA_2_APLIKACJA/SpringApplication/src/test/java/bdbt_bada_project/SpringApplication/ZwierzetaDAOTest.java
@@ -48,11 +48,26 @@ public class ZwierzetaDAOTest {
         Zwierze zwierze = dao.get(id);
         assertNotNull(zwierze);
     }
-      /*
     @Test
-    void testUpdate(){
-        fail("Jeszcze nie zaimplementowano");
+    void testUpdate() throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date utilDate = dateFormat.parse("2023-10-04");
+        java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+        Zwierze zwierze = new Zwierze();
+        zwierze.setNr_zwierzecia(1);
+        zwierze.setData_przyjecia(sqlDate);
+        zwierze.setImie("Reksio");
+        zwierze.setSzczepienie_wscieklizna('T');
+        zwierze.setData_adopcji(null);
+        zwierze.setRok_urodzenia("2022");
+        zwierze.setNr_schroniska(1);
+        zwierze.setNr_adoptujacego(null);
+        zwierze.setNr_kojca(1);
+        zwierze.setNr_rasy(3);
+
+        dao.update(zwierze);
     }
+      /*
     @Test
     void TestDelete(){
         fail("Jeszcze nie zaimplementowano");
